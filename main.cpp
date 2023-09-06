f447a3f02b7271f7cb8e72740f18b3fa7bb7cda5
LaitMax <maksim.slobodchikov21@gmail.com>
2023-09-06 13:04:11 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream> //Подключение потокового ввода/вывода в консоль
+#include <cmath> //Подключение библиотеки cmath
+using namespace std; //Пространство имён std
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x, y, f, s;
+    int i;
+    cout << "Введите значение x >> ";
+    cin >> x;
+    cout << "\nВведите значение y >> ";
+    cin >> y;
+    cout << "\nВыбирите функцию f(x) >> \n1\tsin(x)\n2\tx ^ 2\nelse\te ^ x\n";
+    cin >> i;
+    switch (i)
+    {
+    case 1: cout << "\n Выбрана функция f(x) = sin(x):\t" << sin(x); f = sin(x); break;
+    case 2: cout << "\n Выбрана функция f(x) = x^2:\t" << pow(x, 2); f = pow(x, 2); break;
+    default: cout << "\n Выбрана функция f(x) = e^x:\t" << exp(x); f = exp(x);
+    }
+    if (x > abs(y))
+    {
+        cout << "\n" << x << ">|" << y << "|";
+        s = (2 * pow(f, 3)) + (3 * pow(y, 2));
+    }
+    else if ((3 < x) and (x < abs(y)))
+    {
+        cout << "\n3<" << x << "<|" << y << "|";
+        s = abs(f - y);
+    }
+    else
+    {
+        s = cbrt(abs(f - y));
+    }
+    cout << "\nMagic...";
+    cout << "\n s = " << s;
+}
